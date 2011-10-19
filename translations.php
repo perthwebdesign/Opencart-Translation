98da23c661a377973c261e3433b29adf9b182d0f
Matt Boddy <mattboddy@pwd.net.au>
2011-10-19 16:33:22 +0800

    added translations config file

---
+++ translations.php
@@ -0,0 +1,121 @@
+<?php
+
+	/* Translations */
+
+	
+	Configure::write('Translations', array(
+	
+		//.. DATASOURCES AVAILABLE
+		'OpenCart' => array(
+			//.. TRANSLATABLE OBJECTS
+			'Product' => array(
+				//.. OBJECT IS COMPRISED OF
+				'OpenCartProduct' => array(
+					//.. TRANSLATION SOURCES
+					'LegacyAllTools' => array(
+						"product_id" 		=> array( 'model' => 'LegacyAllToolsProduct', 'column' => 'product_id' ),
+					    "model"				=> array( 'model' => 'LegacyAllToolsProduct', 'column' => 'product_manufacturer_pn' ),
+					    "sku"				=> array( 'model' => 'LegacyAllToolsProduct', 'column' => 'product_manufacturer_pn' ),
+					    "upc"				=> NULL,
+					    "location"			=> NULL,
+					    "quantity"			=> array( 'model' => 'LegacyAllToolsProduct', 'column' => 'product_stock' ),
+					    // "stock_status_id"	=> 5,
+					    "image"				=> NULL,
+					    // "manufacturer_id"	=> NULL,
+					    "shipping"			=> 1,
+					    "price"				=> array( 'model' => 'LegacyAllToolsProduct', 'column' => 'product_price' ),
+					    // "points"			=> NULL,
+					    // "tax_class_id"		=> NULL,
+					    "date_available"	=> "STRING::0000-00-00 00:00:00",
+					    // "weight"			=> NULL,
+					    // "weight_class_id"	=> NULL,
+					    // "length"			=> NULL,
+					    // "width"				=> NULL,
+					    // "height"			=> NULL,
+					    // "length_class_id"	=> NULL,
+					    "subtract"			=> 1,
+					    "minimum"			=> 1,
+					    "sort_order"		=> 1,
+					    "status"			=> 1,
+					    "date_added"		=> "STRING::0000-00-00 00:00:00",
+					    "date_modified"		=> "STRING::0000-00-00 00:00:00",
+					    "viewed"			=> 0,
+					)
+				)
+			)
+		)
+	));
+				
+				
+				
+				
+// 				
+				// 'OpenCartProductDescription',
+					// 'LegacyAllTools' => array(
+						// 'language_id' => 1,
+						// 'name' => 'LegacyAllToolsProduct.product_manufacturer_pn',
+						// 'description' => 'LegacyAllToolsProduct.product_description'],
+						// 'meta_description' => "",
+						// 'meta_keyword' => "",
+					// ),
+// 				
+// 				
+// 				
+				// 'OpenCartCategory',
+// 				
+// 				
+				// 'LegacyAllTools' => array(
+					// // 'OpenCartCategory' => array(
+						// // 'category_id'		=> 'LegacyAllToolsCat.cat_id',
+						// // // 'image'				=>,
+						// // // 'parent_id'			=> 'LegacyAllToolsCat',
+						// // // 'top'				=>,
+						// // // 'column'			=>,
+						// // 'sort_order'		=> 1,
+						// // 'status'			=> 1,
+						// // 'date_added'		=> "STRING::0000-00-00 00:00:00",
+						// // 'date_modified'		=> "STRING::0000-00-00 00:00:00",
+					// // ),
+					// 'OpenCartProduct' => array(
+						// "product_id"		=> array(
+							// 'model'		=> 'LegacyAllToolsProduct',
+							// 'column'	=> 'product_id'
+						// ),
+					    // "model"				=> 'LegacyAllToolsProduct.product_manufacturer_pn',
+					    // "sku"				=> 'LegacyAllToolsProduct.product_manufacturer_pn',
+					    // "upc"				=> NULL,
+					    // "location"			=> NULL,
+					    // "quantity"			=> 'LegacyAllToolsProduct.product_stock',
+					    // // "stock_status_id"	=> 5,
+					    // "image"				=> NULL,
+					    // // "manufacturer_id"	=> NULL,
+					    // "shipping"			=> 1,
+					    // "price"				=> 'LegacyAllToolsProduct.product_price',
+					    // // "points"			=> NULL,
+					    // // "tax_class_id"		=> NULL,
+					    // "date_available"	=> "STRING::0000-00-00 00:00:00",
+					    // // "weight"			=> NULL,
+					    // // "weight_class_id"	=> NULL,
+					    // // "length"			=> NULL,
+					    // // "width"				=> NULL,
+					    // // "height"			=> NULL,
+					    // // "length_class_id"	=> NULL,
+					    // "subtract"			=> 1,
+					    // "minimum"			=> 1,
+					    // "sort_order"		=> 1,
+					    // "status"			=> 1,
+					    // "date_added"		=> "STRING::0000-00-00 00:00:00",
+					    // "date_modified"		=> "STRING::0000-00-00 00:00:00",
+					    // "viewed"			=> 0,
+					// ),
+					// 'OpenCartProductDescription' => array(
+						// 'language_id' => 1,
+						// 'name' => 'LegacyAllToolsProduct.product_manufacturer_pn',
+						// 'description' => 'LegacyAllToolsProduct.product_description'],
+						// 'meta_description' => "",
+						// 'meta_keyword' => "",
+					// ),
+				// )
+			// )
+		// )
+	// ));
